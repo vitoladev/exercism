80c4967dec9173530675385d0ebd9222a5991975
vitoladev <asaptorvi@protonmail.com>
2021-05-03 00:07:32 -0300

    raindrops

---
+++ fsharp/raindrops/Raindrops.fs
@@ -0,0 +1,13 @@
+﻿module Raindrops
+
+
+let convert (number: int) : string =
+    match number with
+    | number when number % 3 = 0 && number % 5 = 0 && number % 7 = 0 -> "PlingPlangPlong"
+    | number when number % 3 = 0 && number % 5 = 0 -> "PlingPlang"
+    | number when number % 3 = 0 && number % 7 = 0 -> "PlingPlong"
+    | number when number % 5 = 0 && number % 7 = 0 -> "PlangPlong"
+    | number when number % 3 = 0 -> "Pling"
+    | number when number % 5 = 0 -> "Plang"
+    | number when number % 7 = 0 -> "Plong"
+    | _ -> number.ToString()
