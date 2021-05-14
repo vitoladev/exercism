d44eb28af26b5b2e91d604526cddcccd9b07c785
vitoladev <asaptorvi@protonmail.com>
2021-05-13 23:35:39 -0300

    rna transcription

---
+++ fsharp/rna-transcription/RnaTranscription.fs
@@ -0,0 +1,12 @@
+﻿module RnaTranscription
+
+let toRna (dna: string) : string =
+    let convertChar char =
+        match char with
+        | 'G' -> 'C'
+        | 'C' -> 'G'
+        | 'T' -> 'A'
+        | 'A' -> 'U'
+        | _ -> char
+
+    dna |> String.map (convertChar)
