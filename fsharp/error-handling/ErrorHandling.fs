852c0f695cb7cfc3f1f7b20ca7b72a6a78e10cf2
Victor Oliveira <asaptorvi@protonmail.com>
2021-08-08 13:44:22 -0300

    fsharp error handling

---
+++ fsharp/error-handling/ErrorHandling.fs
@@ -0,0 +1,25 @@
+﻿module ErrorHandling
+
+open System
+
+let handleErrorByThrowingException () = failwith "error"
+
+let handleErrorByReturningOption (input: string) =
+    let success, number = Int32.TryParse input
+    if success then Some number else None
+
+let handleErrorByReturningResult (input: string) =
+    let success, number = Int32.TryParse input
+
+    if success then
+        Ok number
+    else
+        Error "Could not convert input to integer"
+
+let bind switchFunction twoTrackInput =
+    match twoTrackInput with
+    | Ok n -> switchFunction n
+    | _ -> twoTrackInput
+
+let cleanupDisposablesWhenThrowingException resource =
+    using resource (fun _ -> failwith "error")
